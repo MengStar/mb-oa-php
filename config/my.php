349d5e448fa649f228f13c32ed34989a6f02ff92
MengStar <641510128@qq.com>
2018-05-25 22:09:09 +0800

    first blood

---
+++ config/my.php
@@ -0,0 +1,10 @@
+<?php
+$config->installed    = true;
+$config->debug        = false;
+$config->requestType  = 'PATH_INFO';
+$config->db->host     = 'aliyun.com';
+$config->db->port     = '3306';
+$config->db->name     = 'xxb';
+$config->db->user     = 'root';
+$config->db->password = 'password';
+$config->db->prefix   = '';
