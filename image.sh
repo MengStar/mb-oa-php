910c5db93dab1b4a83a77bfc315e67e53d212684
MengStar <641510128@qq.com>
2018-05-29 12:46:59 +0800

    ensure image available

--- image.sh
+++ image.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
-docker build -t registry.cn-hangzhou.aliyuncs.com/mengstar/mb-oa . &&
+#利用插件生成可执行镜像
+#先生成一个可执行的镜像mb-oa，然后commit，在提交
+docker commit mb-oa registry.cn-hangzhou.aliyuncs.com/mengstar/mb-oa   &&
 docker push registry.cn-hangzhou.aliyuncs.com/mengstar/mb-oa
